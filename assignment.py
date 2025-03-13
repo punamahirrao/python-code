1b6f1b674de8e6fd3b76d74a8a78b8baa0e9814c
Punam <punam.ahirrao@gmail.com>
2025-03-13 10:13:51 +0100

    Python code

---
+++ assignment.py
@@ -0,0 +1,69 @@
+#num1 = int(input ("enter first number :"))
+#num2 = int(input ("enter second number :"))
+#print("sum of the two number is :",num1+num2)
+
+#side = float(input("enter side : "))
+#print("Area of square is :",side * side)
+#------------------------------------------------------------------------
+
+"""name = input("Enter name : ")
+print("length of name is :",len(name))
+
+str = "my name is punam."
+print(len(str))
+print("occurence of letter :",str.count("a"))
+
+print(str.capitalize())
+
+print(str[3])
+
+print(str[1:6])
+
+print(str[-9:-1])
+
+print(str.endswith("am."))
+
+print(str.replace("a","o"))
+
+print(str.count("am"))"""
+#-----------------------------------------------------------------
+"""
+num = int(input("enter number :"))
+
+if(num % 2 == 0):
+    print("number is even")
+else:
+ print("number is odd")  """
+
+"""a = int(input("first num: "))
+b = int(input("second num: "))
+c = int(input("third num: "))
+d = int(input("fourth num: "))
+if(a>=b and a>=c):
+    print("first num is greatest")
+elif(b>=c and b>=d):
+    print("second num is greatest") 
+elif(c>=d):
+    print("third num is greatest")   
+else:
+    print("fourth num is greatest")   
+"""
+#------------------------------------------------------------
+
+"""  movies = []
+(movies.append(input("enter movie1: ")))
+(movies.append(input("enter movie2: ")))
+(movies.append(input("enter movie3: ")))
+print(movies)  """
+#--------------------------------------------------------------
+
+list = [1, 2, 3]
+copy_list = pal.copy()
+copy_list.reverse()
+if (list==copy_list):
+    print("list is palindrom: ", list)
+else:
+    print("list is not palindrom")    
+
+
+
---
+++ my-first-program.py
@@ -0,0 +1,21 @@
+name="Punam"
+age =24
+
+#print("my name is:",name)
+#print ("my age is:",age)
+
+#num1 = 10
+#num2 = 20
+#sum = num1+num2
+#print("sum of two num : ",sum)
+
+a=5
+b=2
+print(a**b)
+
+name = input("enter your name :")
+print("Welcome",name)
+print(type(name),name)
+
+
+
